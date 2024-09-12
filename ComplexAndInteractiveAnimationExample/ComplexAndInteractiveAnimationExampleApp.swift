9daaff46cba1e93a7218bbd1f0ca6a50a358cd3a
fumiyasac <just1factory@gmail.com>
2024-09-12 22:20:35 +0900

    first commit

---
+++ ComplexAndInteractiveAnimationExample/ComplexAndInteractiveAnimationExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComplexAndInteractiveAnimationExampleApp.swift
+//  ComplexAndInteractiveAnimationExample
+//
+//  Created by 酒井文也 on 2024/09/12.
+//
+
+import SwiftUI
+
+@main
+struct ComplexAndInteractiveAnimationExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
